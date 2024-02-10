4bbe50c4a2133054e670d06498512d4789092501
mango_yuzi <1183437162@qq.com>
2024-02-10 23:12:28 +0800

    数据模型Demo

---
+++ Code-Generator-basic/src/main/java/com/mango/freemaker/dataModel/MainTemplateData.java
@@ -0,0 +1,14 @@
+package com.mango.freemaker.dataModel;
+
+import lombok.Data;
+
+/**
+ *
+ */
+@Data
+public class MainTemplateData {
+    private  boolean loop;
+    private String author="mango";
+
+    private String message="sum：";
+}
