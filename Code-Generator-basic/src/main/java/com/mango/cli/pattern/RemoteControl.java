b66b0733ea8bdd37c14cdbeb0d4849800af65878
mango_yuzi <1183437162@qq.com>
2024-02-11 21:27:53 +0800

    添加注释

--- Code-Generator-basic/src/main/java/com/mango/cli/pattern/RemoteControl.java
+++ Code-Generator-basic/src/main/java/com/mango/cli/pattern/RemoteControl.java
@@ -2,7 +2,7 @@ package com.mango.cli.pattern;
 
 public class RemoteControl {
     private Command command;
-
+    //想象成给遥控器添加按钮
     public void setCommand(Command command) {
         this.command = command;
     }
--- Code-Generator-basic/src/main/java/com/mango/cli/pattern/TurnOffCommand.java
+++ Code-Generator-basic/src/main/java/com/mango/cli/pattern/TurnOffCommand.java
@@ -1,12 +1,14 @@
 package com.mango.cli.pattern;
 
 public class TurnOffCommand implements Command {
+    //给命令绑定一个设备。
     private Device device;
 
     public TurnOffCommand(Device device){
         this.device = device;
     }
 
+    //调用设备的方法
     public void execute(){
         device.turnOff();
     }
