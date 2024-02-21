680ae05a58cf0411f8ebf3f1036c220d475b587e
mango_yuzi <1183437162@qq.com>
2024-02-21 17:54:11 +0800

    启动程序

---
+++ code-generator-maker/src/main/java/com/mango/maker/Main.java
@@ -0,0 +1,12 @@
+package com.mango.maker;
+
+import com.mango.maker.generator.main.MainGenerator;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
+        MainGenerator.doGenerate();
+    }
+}
