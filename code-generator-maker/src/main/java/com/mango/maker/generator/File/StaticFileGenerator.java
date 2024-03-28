60e35bd123e8974d1c3b2c04da23bd7f98434a78
Albert <11346920+yz_z1@user.noreply.gitee.com>
2024-03-29 00:25:30 +0800

    优化第二阶段生成静态与动态文件的逻辑

---
+++ code-generator-maker/src/main/java/com/mango/maker/generator/File/StaticFileGenerator.java
@@ -0,0 +1,18 @@
+package com.mango.maker.generator.File;
+
+import cn.hutool.core.io.FileUtil;
+
+/**
+ * 生成静态文件
+ */
+public class StaticFileGenerator {
+    /**
+     * 使用hutool拷贝目录，生成静态文件
+     *
+     * @param inputPath  输入目录
+     * @param outputPath 输出目录
+     */
+    public static void copyStaticFileByHutool(String inputPath, String outputPath) {
+        FileUtil.copy(inputPath, outputPath, false);
+    }
+}
