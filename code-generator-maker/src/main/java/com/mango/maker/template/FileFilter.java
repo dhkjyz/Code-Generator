92f6169dbaacee3ab705ef0d854ea06ddcbffe76
Albert <11346920+yz_z1@user.noreply.gitee.com>
2024-03-29 09:20:01 +0800

    第二阶段，生成文件模版时，文件过滤器

--- code-generator-maker/src/main/java/com/mango/maker/template/FileFilter.java
+++ code-generator-maker/src/main/java/com/mango/maker/template/FileFilter.java
@@ -2,7 +2,7 @@ package com.mango.maker.template;
 
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.io.FileUtil;
-import com.mango.maker.template.model.FileFilterConfig;
+import com.mango.maker.template.model.TemplateMakeFileConfig;
 import com.mango.maker.template.model.enums.FileRegularFilterEnum;
 import com.mango.maker.template.model.enums.FileTypeFilterEnum;
 
@@ -12,36 +12,38 @@ import java.util.stream.Collectors;
 
 public class FileFilter {
     /**
-     * 对某个文件/目录进行过滤，返回已过滤后的文件列表，再把列表放到制作模板的方法中
+     * 文件过滤器
+     *
      * @param filePath
      * @param fileFilterConfigList
      * @return
      */
-    public static List<File> doFilter(String filePath , List<FileFilterConfig> fileFilterConfigList){
+    public static List<File> doFilter(String filePath, List<TemplateMakeFileConfig.FileFilterConfig> fileFilterConfigList) {
 
-        List<File> fileList = FileUtil.loopFiles(filePath);
+        List<File> fileList = FileUtil.loopFiles(filePath);//无论是文件还是目录，统一变成文件列表。
 
         return fileList.stream()
-                .filter(file-> doSingleFileFilter(fileFilterConfigList,file))
+                .filter(file -> doSingleFileFilter(fileFilterConfigList, file))
                 .collect(Collectors.toList());
     }
 
     /**
      * 对单一文件进行过滤
-     * @param filterConfigList
-     * @param inputFileAbsolutePath
+     *
+     * @param filterConfigList      过滤规则
+     * @param inputFileAbsolutePath 要过滤文件的绝对值
      * @return
      */
-    public static boolean doSingleFileFilter(List<FileFilterConfig> filterConfigList , File inputFileAbsolutePath){
-        String fileName = inputFileAbsolutePath .getName();
-        String fileContent = FileUtil.readUtf8String(inputFileAbsolutePath);
+    public static boolean doSingleFileFilter(List<TemplateMakeFileConfig.FileFilterConfig> filterConfigList, File inputFileAbsolutePath) {
+        String fileName = inputFileAbsolutePath.getName(); //读取要过滤文件的标题
+        String fileContent = FileUtil.readUtf8String(inputFileAbsolutePath); //读取要过滤文件的内容
         boolean result = true;
 
-        if (CollUtil.isEmpty(filterConfigList)){
+        if (CollUtil.isEmpty(filterConfigList)) {
             return true;
         }
         String content = null;
-        for (FileFilterConfig fileFilterConfig : filterConfigList) {
+        for (TemplateMakeFileConfig.FileFilterConfig fileFilterConfig : filterConfigList) {
             String rule = fileFilterConfig.getRule();
             String value = fileFilterConfig.getValue();
             String range = fileFilterConfig.getRange();
@@ -50,12 +52,12 @@ public class FileFilter {
             if (enumTypeByValue == null) {
                 continue;
             }
-            switch (enumTypeByValue){
+            switch (enumTypeByValue) {
                 case FILE_NAME:
-                    content = fileName ;
+                    content = fileName;
                     break;
                 case FILE_CONTENT:
-                    content = fileContent ;
+                    content = fileContent;
                     break;
                 default:
             }
@@ -65,7 +67,7 @@ public class FileFilter {
                 continue;
             }
 
-            switch (enumRegularByValue){
+            switch (enumRegularByValue) {
                 case START_WITH:
                     result = content.startsWith(value);
                     break;
@@ -79,11 +81,11 @@ public class FileFilter {
                     result = content.equals(value);
                     break;
                 case REGEX:
-                    result= content.matches(value);
+                    result = content.matches(value);
                     break;
             }
 
-            if (!result){
+            if (!result) {
                 return false;
             }
         }
